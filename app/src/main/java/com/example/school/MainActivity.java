5e7e4b882409ab1684cc2e8b7049f18fa8bc0bf9
karishma pradhan <karishmapradhan9090@gmail.com>
2024-10-14 20:07:11 +0530

    new 4/2024

--- app/src/main/java/com/example/school/MainActivity.java
+++ app/src/main/java/com/example/school/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.school;
 
+import android.content.Intent;
 import android.icu.text.SimpleDateFormat;
 import android.os.Bundle;
 import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -15,6 +18,7 @@ import java.util.Date;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn;
 
     TextView textView,textView2;
 
@@ -23,25 +27,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        btn=findViewById(R.id.b1);
 
         textView=findViewById(R.id.cdatetime);
         textView2=findViewById(R.id.locaitontxtview);
         String date = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
         textView.setText(date);
-
-
-
-
-
-
-
-
-
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, List.class));
+            }
         });
 
 
